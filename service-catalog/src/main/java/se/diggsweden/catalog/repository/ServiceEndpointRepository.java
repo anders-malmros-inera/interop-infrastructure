25452062921bf86a8efd72530bb534023f383023
Anders Malmros <anders.malmros@inera.se>
2025-11-01 11:51:49 +0100

    restart

--- service-catalog/src/main/java/se/diggsweden/catalog/repository/ServiceEndpointRepository.java
+++
@@ -1,9 +0,0 @@
-package se.diggsweden.catalog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.diggsweden.catalog.model.ServiceEndpoint;
-
-import java.util.UUID;
-
-public interface ServiceEndpointRepository extends JpaRepository<ServiceEndpoint, UUID> {
-}
--- service-catalog/src/main/java/se/diggsweden/catalog/repository/ServiceEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package se.diggsweden.catalog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.diggsweden.catalog.model.ServiceEntry;
-
-import java.util.UUID;
-
-public interface ServiceEntryRepository extends JpaRepository<ServiceEntry, UUID> {
-}
